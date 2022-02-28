1c6451ca7bd9d18854adc6b06e9c1d496f5c230f
wsblues <37092601+wsblues@users.noreply.github.com>
2022-02-28 18:56:58 +0900

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,56 @@
+from tqdm import tqdm
+import torch 
+def final_loss(bce_loss, mu, logvar):
+    """
+    This function will add the reconstruction loss (BCELoss) and the 
+    KL-Divergence.
+    KL-Divergence = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    :param bce_loss: recontruction loss
+    :param mu: the mean from the latent vector
+    :param logvar: log variance from the latent vector
+    """
+    BCE = bce_loss 
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return BCE + KLD
+
+
+
+def train(model, dataloader, dataset, device, optimizer, criterion):
+    model.train()
+    running_loss = 0.0
+    counter = 0
+    for i, data in tqdm(enumerate(dataloader), total=int(len(dataset)/dataloader.batch_size)):
+        counter += 1
+        data = data[0]
+        data = data.to(device)
+        optimizer.zero_grad()
+        reconstruction, mu, logvar = model(data)
+        bce_loss = criterion(reconstruction, data)
+        loss = final_loss(bce_loss, mu, logvar)
+        loss.backward()
+        running_loss += loss.item()
+        optimizer.step()
+    train_loss = running_loss / counter 
+    return train_loss
+
+
+
+def validate(model, dataloader, dataset, device, criterion):
+    model.eval()
+    running_loss = 0.0
+    counter = 0
+    with torch.no_grad():
+        for i, data in tqdm(enumerate(dataloader), total=int(len(dataset)/dataloader.batch_size)):
+            counter += 1
+            data= data[0]
+            data = data.to(device)
+            reconstruction, mu, logvar = model(data)
+            bce_loss = criterion(reconstruction, data)
+            loss = final_loss(bce_loss, mu, logvar)
+            running_loss += loss.item()
+        
+            # save the last batch input and output of every epoch
+            if i == int(len(dataset)/dataloader.batch_size) - 1:
+                recon_images = reconstruction
+    val_loss = running_loss / counter
+    return val_loss, recon_images
